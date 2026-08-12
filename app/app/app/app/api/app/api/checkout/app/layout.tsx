4dd766179d190c79bb1ca454049a3d15aa4c25d5
Rennius-ne <renaldas473@gmail.com>
2026-08-12 04:08:51 +0300

    Create layout.tsx

---
+++ app/app/app/app/api/app/api/checkout/app/layout.tsx
@@ -0,0 +1,11 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "OnlineStore",
+  "name": "QuickMind",
+  "description": "AI produktai, kurie taupo laiką ir padede uždirbti daugiau.",
+  "url": "https://quickmind.lt",
+  "currenciesAccepted": "EUR",
+  "paymentAccepted": "Credit Card, PayPal, Revolut Pay"
+}
+</script>
